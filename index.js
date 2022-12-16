1e5cdb3fc986c477cc69f076f11e296c81695178
mridul-netizen <mridulsetia18@gmail.com>
2022-12-16 23:15:12 +0530

    remove warning

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const lodash=require("lodash");
 const length=100;
+mongoose.set('strictQuery', true);
 
 mongoose.connect("mongodb+srv://dhruv:Dhruv@cluster0.rapcoui.mongodb.net/blogDB")
 const app = express();
@@ -32,7 +33,6 @@ const ListSchema={
 
 const Post=mongoose.model("Post",PostSchema);
 const List=mongoose.model("List",ListSchema);
-
 // structured fixed contact
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 
